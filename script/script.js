9165257970242018b63748dfee7b2a9a59236a20
kostia89 <0665761301k@gmail.com>
2021-12-19 19:04:12 +0200

    slider

---
+++ script/script.js
@@ -0,0 +1,26 @@
+$(document).ready (function(){
+    $ ('.trust_logo').slick({
+        infinite: true,
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        
+        responsive: [
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        slidesToShow: 2,
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        slidesToShow: 1,
+	      }
+	    }
+    ]
+    })
+});
+  console.log('ok');
